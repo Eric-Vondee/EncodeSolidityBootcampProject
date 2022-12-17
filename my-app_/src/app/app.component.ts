875206b7631f3f84b5bb12b2f359d6cd6b008e80
Eno Yda <info@fadyone.com>
2022-12-17 19:09:45 +0100

    Add files via upload

--- my-app_/src/app/app.component.ts
+++ my-app_/src/app/app.component.ts
@@ -1,16 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import { AppService } from 'project/project-name/src/app.service.ts';
+import { AppService } from '../../../project-name/src/app.service';
+import { Observable, of } from 'rxjs';
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  score: number;
-  totalQuestions: number;
-  currentQuestion: number;
-  questions: string[];
-  answers: string[][];
+  score?: number;
+  totalQuestions?: number;
+  // currentQuestion: number  = 0;
+  currentQuestion?: number; // you can either initialize these properties with a default value, or mark them as undefined by adding a ? after the type declaration.
+  questions?: string[];
+  answers?: string[][];
 
   constructor(private appService: AppService) {}
 
@@ -54,68 +58,3 @@ export class AppComponent implements OnInit {
 
 
 
-
-
-// import { Component, OnInit } from '@angular/core';
-// import { providers, Contract, utils } from 'ethers';
-// import Quiz from '/Users/one/Documents/HARDHAT/EncodeSolidityBootcampProject/packages/blockchain/contracts/Quiz.sol ';
-
-// @Component({
-//   selector: 'app-app',
-//   templateUrl: './app.component.html',
-//   styleUrls: ['./app.component.scss']
-// })
-// export class QuizComponent implements OnInit {
-//   score: number;
-//   totalQuestions: number;
-//   currentQuestion: number;
-//   questions: string[];
-//   answers: string[][];
-//   correctAnswers: number[];
-
-//   private provider: providers.JsonRpcProvider;
-//   private contract: Contract;
-
-//   constructor() {
-//     // Initialize provider and contract
-//     this.provider = new providers.JsonRpcProvider('http://localhost:8545');
-//     this.contract = new Contract(
-//       Quiz.address,
-//       Quiz.abi,
-//       this.provider.getSigner()
-//     );
-//   }
-
-//   ngOnInit(): void {
-//     // Get the current score, total number of questions, and current question index
-//     this.contract.score().then(score => {
-//       this.score = score;
-//     });
-//     this.contract.totalQuestions().then(totalQuestions => {
-//       this.totalQuestions = totalQuestions;
-//     });
-//     this.contract.currentQuestion().then(currentQuestion => {
-//       this.currentQuestion = currentQuestion;
-//     });
-
-//     // Get the list of questions and answers
-//     this.contract.totalQuestions().then(numQuestions => {
-//       for (let i = 0; i < numQuestions; i++) {
-//         this.contract.questions(i).then(question => {
-//           this.questions.push(question);
-//         });
-//         this.contract.answers(i).then(answers => {
-//           this.answers.push(answers);
-//         });
-//         this.contract.correctAnswers(i).then(correctAnswer => {
-//           this.correctAnswers.push(correctAnswer);
-//         });
-//       }
-//     });
-//   }
-
-//   async submitAnswer(question: number, answer: number): Promise<void> {
-//     // Submit the answer to the contract
-//     await this.contract.submitAnswer(question, answer);
-//   }
-// }
