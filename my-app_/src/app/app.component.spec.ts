562fa989d058223962f83f578e43919e8845bdf0
Eno Yda <info@fadyone.com>
2022-12-20 01:39:22 +0100

    Add files via upload

--- my-app_/src/app/app.component.spec.ts
+++ my-app_/src/app/app.component.spec.ts
@@ -1,104 +1,31 @@
-// import { TestBed } from '@angular/core/testing';
-// import { AppComponent } from './app.component';
-// // import { AppService } from 'project/project-name/src/app.service.ts';
-// import { AppService } from '../../../project-name/src/app.service';
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
 
-// import { of } from 'rxjs';
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  });
 
-// describe('AppComponent', () => {
-//   let component: AppComponent;
-//   let appService: AppService;
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
 
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [AppComponent],
-//       providers: [AppService],
-//     });
+  it(`should have as title 'my-app'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('my-app');
+  });
 
-//     component = TestBed.inject(AppComponent);
-//     appService = TestBed.inject(AppService);
-//   });
-
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-
-//   it('should get the current score from the service', () => {
-//     const score = 10;
-//     spyOn(appService, 'getScore').and.returnValue(of(score));
-//     component.ngOnInit();
-//     expect(component.score).toBe(score);
-//   });
-
-//   it('should get the total number of questions from the service', () => {
-//     const totalQuestions = 5;
-//     spyOn(appService, 'getTotalQuestions').and.returnValue(of(totalQuestions));
-//     component.ngOnInit();
-//     expect(component.totalQuestions).toBe(totalQuestions);
-//   });
-
-//   it('should get the current question index from the service', () => {
-//     const currentQuestion = 3;
-//     spyOn(appService, 'getCurrentQuestion').and.returnValue(of(currentQuestion));
-//     component.ngOnInit();
-//     expect(component.currentQuestion).toBe(currentQuestion);
-//   });
-
-//   it('should get the list of questions from the service', () => {
-//     const questions = ['What is the capital of France?', 'What is the capital of Japan?'];
-//     spyOn(appService, 'getQuestions').and.returnValue(of(questions));
-//     component.ngOnInit();
-//     expect(component.questions).toEqual(questions);
-//   });
-
-//   it('should get the list of answers from the service', () => {
-//     const answers = [['Paris', 'Tokyo', 'New York'], ['Tokyo', 'Paris', 'Beijing']];
-//     spyOn(appService, 'getAnswers').and.returnValue(of(answers));
-//     component.ngOnInit();
-//     expect(component.answers).toEqual(answers);
-//   });
-
-//   it('should submit the answer to the service when the user clicks a button', () => {
-//     const question = 0;
-//     const answer = 0;
-//     spyOn(appService, 'submitAnswer').and.returnValue(of(null));
-//     component.submitAnswer(question, answer);
-//     expect(appService.submitAnswer).toHaveBeenCalledWith(question, answer);
-//   });
-// });
-
-
-
-
-
-// // import { TestBed } from '@angular/core/testing';
-// // import { AppComponent } from './app.component';
-
-// // describe('AppComponent', () => {
-// //   beforeEach(async () => {
-// //     await TestBed.configureTestingModule({
-// //       declarations: [
-// //         AppComponent
-// //       ],
-// //     }).compileComponents();
-// //   });
-
-// //   it('should create the app', () => {
-// //     const fixture = TestBed.createComponent(AppComponent);
-// //     const app = fixture.componentInstance;
-// //     expect(app).toBeTruthy();
-// //   });
-
-// //   it(`should have as title 'my-app'`, () => {
-// //     const fixture = TestBed.createComponent(AppComponent);
-// //     const app = fixture.componentInstance;
-// //     expect(app.title).toEqual('my-app');
-// //   });
-
-// //   it('should render title', () => {
-// //     const fixture = TestBed.createComponent(AppComponent);
-// //     fixture.detectChanges();
-// //     const compiled = fixture.nativeElement as HTMLElement;
-// //     expect(compiled.querySelector('.content span')?.textContent).toContain('my-app app is running!');
-// //   });
-// // });
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.content span')?.textContent).toContain('my-app app is running!');
+  });
+});
--- my-app_/src/app/app.component.ts
+++ my-app_/src/app/app.component.ts
@@ -1,60 +1,56 @@
-import { Component, OnInit } from '@angular/core';
-import { AppService } from '../../../project-name/src/app.service';
-import { Observable, of } from 'rxjs';
-
+import { Component } from '@angular/core';
+import { ethers } from 'ethers';
+import { WalletService } from './services/wallet.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  score?: number;
-  totalQuestions?: number;
-  // currentQuestion: number  = 0;
-  currentQuestion?: number; // you can either initialize these properties with a default value, or mark them as undefined by adding a ? after the type declaration.
-  questions?: string[];
-  answers?: string[][];
+export class AppComponent {
 
-  constructor(private appService: AppService) {}
+  submitAnswer() {
+  throw new Error('Method not implemented.');
+}
+  public walletConnected: boolean = false;
+  public walletId: string = '';
+  provider: ethers.providers.Provider;
 
-  ngOnInit() {
-    this.appService.getScore().subscribe((score) => {
-      this.score = score;
-    });
+  title = 'Proof your Knowledge and get an exclusive NFT';
+  number = 44;
+  score= 0;
+  answers: string[] | undefined;
+  totalQuestions: number | undefined;
+  currentQuestion: number | undefined;
+  userAnswer: number | undefined;
+  question: number | undefined ;
 
-    this.appService.getTotalQuestions().subscribe((totalQuestions) => {
-      this.totalQuestions = totalQuestions;
-    });
 
-    this.appService.getCurrentQuestion().subscribe((currentQuestion) => {
-      this.currentQuestion = currentQuestion;
-    });
-
-    this.appService.getQuestions().subscribe((questions) => {
-      this.questions = questions;
-    });
-
-    this.appService.getAnswers().subscribe((answers) => {
-      this.answers = answers;
-    });
+  constructor(private walletService: WalletService) {
+    this.provider = ethers.providers.getDefaultProvider("goerli");
+    this.connectToWallet  = async () => {
+      const accounts = await this.walletService.connectWallet();
+      if(accounts.length > 0){
+        this.walletConnected = true;
+        this.walletId = accounts[0];
+      }
+    }
+    this.currentQuestion = 0;
+    this.totalQuestions = 0;
+    this.userAnswer = 0;
+    this.answers = ["Option 1","Option 2","Option 3","Option 4"];
   }
+  connectToWallet  = async () => {
 
-  submitAnswer(question: number, answer: number) {
-    this.appService.submitAnswer(question, answer).subscribe(() => {
-      // update the score and current question
-      this.appService.getScore().subscribe((score) => {
-        this.score = score;
-      });
-
-      this.appService.getCurrentQuestion().subscribe((currentQuestion) => {
-        this.currentQuestion = currentQuestion;
-      });
-    });
+  }  
+  checkWalletConnected = async () => {
+    const accounts = await this.walletService.checkWalletConnected();
+    if(accounts.length > 0){
+      this.walletConnected = true;
+      this.walletId = accounts[0];
+    }
+  }
+  ngOnInit(): void {
+    this.checkWalletConnected();
   }
 }
-
-
-
-
-
